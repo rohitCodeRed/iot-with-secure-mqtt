420f138a1ffe40031a997c9e25a40063a6a8955d
rohitCodeRed <rohit06051993@gmail.com>
2022-12-03 00:03:24 +0530

    Socket connection tested..

--- app.js
+++ app.js
@@ -6,24 +6,32 @@ const websocket = require("ws");
 const {handle_publish} = require("./mqtt_publisher");
 const {handle_subscription} = require("./mqtt_subscriber");
 
+app.set('view engine', 'ejs');
+
 
 const SOCKET_CLIENT_CONNECTIONS =[];
 
-const MQTT_URL = "mqtt://192.168.8.103";
+const MQTT_URL = "mqtt://192.168.8.101";
 const MQTT_PORT ="3000";
-const SERVER_URL = "http://192.168.8.103";
+const SERVER_IP = "192.168.8.101";
+const SERVER_SOCKET_ENDPOINT="iot_sensors";
+const SERVER_URL = "http://192.168.8.101";
 const SERVER_PORT = 4000;
 
 const TOPICS=["iot_temperature","iot_humidity","iot_vibration","rgb_light"];
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: TEMPALTE_PATH});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
+    //res.sendFile('index.html', {root: TEMPALTE_PATH});      //server responds by sending the index.html file to the client's browser
+    res.render("pages/index",{
+        "ip": SERVER_IP,
+        "port":SERVER_PORT,
+        "endPoint": SERVER_SOCKET_ENDPOINT
+      })                                                    //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
 const server  = app.listen(SERVER_PORT, () => {            //server starts listening for any attempts from a client to connect at port: {port}
-    console.log(`Now listening on port ${port}`); 
+    console.log(`Now listening on port ${SERVER_PORT}`); 
 });
 
 
@@ -64,28 +72,28 @@ mConnection.on('message', function (topic, message) {
     //client.end()
   });
 
-connection.on("error",function(error){ 
+mConnection.on("error",function(error){ 
     console.log("Can't connect"+error);
+    mConnection.end();
 });
 
 
 //----------web socket...........
-const wss = new websocket.Server({server:server,path:"/iot-sensors"});
+const wss = new websocket.Server({server:server,path:"/"+SERVER_SOCKET_ENDPOINT});
 
 wss.on("connection",(ws,req)=>{
-
-    //SOCKET_CLIENT_CONNECTIONS.set(req.clientId,ws);
+    console.log("Websocket working... client info:- ",req.headers.host," : ",req.headers['user-agent']);
     SOCKET_CLIENT_CONNECTIONS.push(ws);
-    ws.send("Welcome to the websocket server!!");
 
     ws.on("message",(msg)=>{
-        console.log("Websocket working...",msg);
+        
         let pushData = {
             topic:TOPICS[3],
             data:msg
         }
+        ws.send(JSON.stringify(pushData));
 
-        handle_publish(mConnection,ws,pushData);
+        //handle_publish(mConnection,ws,pushData);
         //TODO from web site...
     });
 });
